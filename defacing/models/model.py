30930a9bf51a28a6ee2cfd7066f03c5d878a2f52
koriavinash1 <koriavinash1@gmail.com>
2020-02-23 14:00:46 +0530

    model updated
    Former-commit-id: 4d3d060739d4063c23da69b22f3e987530efa8c9
    Former-commit-id: 4aa9b854427d663ab329eae5c666a5279dd37493 [formerly 109c19985d0e25d0ca8e767152c7f01e37f5f126]
    Former-commit-id: eb301355bfef72f6aeab3c11996a14faa118a26e

--- defacing/models/model.py
+++ defacing/models/model.py
@@ -48,23 +48,21 @@ def custom_model(input_shape=(32, 32), dropout=0.4, nclasses=None, multiencoders
     """
 
     inp1 = layers.Input(shape=input_shape + (1,), name='sagittal')
-    #inp2 = layers.Input(shape=input_shape + (1,), name='axial')
-    #inp3 = layers.Input(shape=input_shape + (1,), name='corronal')
+    inp2 = layers.Input(shape=input_shape + (1,), name='axial')
+    inp3 = layers.Input(shape=input_shape + (1,), name='corronal')
 
     sagittal = submodel(input_shape)
-    """
     if multiencoders:
         axial = submodel(input_shape)
         corronal = submodel(input_shape)
 
-        merge = [sagittal(inp1), axial(inp1), corronal(inp1)]
+        merge = [sagittal(inp1), axial(inp2), corronal(inp3)]
     else:
         merge = [sagittal(inp1), sagittal(inp2), sagittal(inp3)]
 
     concat = layers.Add()(merge)
-    """
-    out = layers.Dense(256, activation='relu')(sagittal(inp1))
+    out = layers.Dense(256, activation='relu')(concat)
     out = layers.Dropout(dropout)(out)
 
     out = layers.Dense(1, activation='sigmoid', name='output_node')(out)
-    return models.Model(inputs=inp1, outputs=out)
+    return models.Model(inputs=[inp1, inp2, inp3], outputs=out)
