57cbb83c9d45a794e42decb778d0da0d276122c5
koriavinash1 <koriavinash1@gmail.com>
2020-02-23 13:59:36 +0530

    gradcam results
    Former-commit-id: 4fca06197b858b84f65c3cb200f703b498a0d984
    Former-commit-id: 447c7630acd14a5362fc195a071ed34742aee740 [formerly 13456c77cd2a9b42f720f153bb4cf2a7f79a07ee]
    Former-commit-id: 04bd95b7eecbbd5e93f6cf70ba8674475aeb39e3

--- examples/cam_analysis.py
+++ examples/cam_analysis.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 sys.path.append('..')
 from defacing.helpers.utils import load_volume
 from defacing.dataloaders.inference_dataloader import DataGeneratoronFly
@@ -13,36 +13,51 @@ from keras import activations
 import vis
 from vis.visualization import visualize_cam
 from vis.utils import utils
-
+import keras.backend as K
+import tensorflow as tf
 
 volume_path = '../sample_vols/faced/example3.nii.gz'
 volume = load_volume(volume_path)
 
 
 model_path = '../defacing/saved_weights/best_cv2.h5'
-model = custom_model(input_shape = (64,64),nclasses=2, multiencoders=True)
-# model.load_weights(model_path, by_name=True)
+model = custom_model(input_shape = (64,64), nclasses=2, multiencoders=True)
+session = K.get_session()
+init = tf.global_variables_initializer()
+session.run(init)
+model.load_weights(model_path, by_name=True)
 
 print (model.summary())
 dataloader_params = {'image_size': 64, 'nchannels': 1, 'nmontecarlo':1, 'transform':None}
 datagenerator = DataGeneratoronFly(**dataloader_params)
 X1, _, _ = datagenerator.get_data(volume)
 X2 = np.array(X1[0])
-
+score = np.squeeze(model.predict(X2))
+print(score)
 save_path = '../cam_results'
 
-for layer in range(1, len(model.layers)):
-    print (X2[0, 0].shape, model.layers[layer].name)
-    if model.layers[layer].name not in ['dense', 'dropout', 'output_node']: continue
     
-    if save_path:
-        plt.figure(figsize=(30, 20))
-        gs = gridspec.GridSpec(2, 3)
-        gs.update(wspace=0.025, hspace=0.05)
+if save_path:
+    os.makedirs(save_path, exist_ok=True)
+    plt.figure(figsize=(10, 10))
+    gs = gridspec.GridSpec(1, 1)
+    gs.update(wspace=0.025, hspace=0.05)
 
-    for class_ in range(2):
-        grads_ = visualize_cam(model, -1, filter_indices=class_, penultimate_layer_idx = 0, 
-                                seed_input = X2)
-        grads_ = np.array(grads_)
-        print (X2.shape, grads_.shape)
+grads_ = visualize_cam(model, -1, filter_indices=0, penultimate_layer_idx = 0, 
+                       seed_input = X2)
+grads_ = np.array(grads_)
+ax = plt.subplot(gs[0,0])
+im = ax.imshow(np.rot90(np.squeeze(X2)), cmap='gray')
+im = ax.imshow(np.rot90(grads_), alpha=0.5, cmap='jet')
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+ax.set_aspect('equal')
+ax.tick_params(bottom='off', top='off', labelbottom='off' )
+ax.set_title("confidence: {0:.2f}".format(100*float(score)))
+divider = make_axes_locatable(ax)
+cax = divider.append_axes("right", size="5%", pad=0.2)
+cb = plt.colorbar(im, ax=ax, cax=cax )
+
+plt.savefig(os.path.join(save_path, 'cam_example.png'), tight_box=True)
+print (X2.shape, grads_.shape)
 
